777d3d57454c9f4a0a0ba0f587bd5b1995f396bb
Ebenezer Ejukorlem <talk2benzo@yahoo.com>
2018-07-29 09:03:11 +0100

    Completed backend

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,10 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
+
+// Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 // Load User model
 const User = require("../../models/User");
 
@@ -18,9 +22,16 @@ router.get("/test", (req, res) => res.json({ msg: "user route works" }));
 // @desc    Tests users route
 // @access  Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      errors.email = "Email already exists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravater.url(req.body.email, {
         s: "200", // Size of gravater
@@ -53,6 +64,13 @@ router.post("/register", (req, res) => {
 // @desc    Return JWT to authenticated user trying to login
 // @access  public
 router.post("/login", (req, res) => {
+  //Validate login input
+  const { errors, isValid } = validateLoginInput(req.body);
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -60,7 +78,8 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then(user => {
     // Check for user
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+      errors.email = "User not found";
+      return res.status(404).json(errors);
     }
 
     // Check Password
@@ -81,7 +100,8 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: "Password incorrect" });
+        errors.password = "Password is incorrect";
+        return res.status(400).json(errors);
       }
     });
   });
